8e98d23a68d04b934e4db16bebc5fb4ab1f3b797
Ariel Seijo <hariel.seijo@gmail.com>
2026-03-27 21:33:05 -0300

    agrego el contexto del carrito

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { CartProvider } from "@/context/CartContext";
 import Navbar from "./components/Navbar";
 
 export default function RootLayout({ children }) {
@@ -6,7 +7,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Navbar />
-        {children}
+        <CartProvider>{children}</CartProvider>
       </body>
     </html>
   );

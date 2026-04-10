d4e1a85c86c80547d00524da22426bb3a1fe7ad0
Ariel Seijo <hariel.seijo@gmail.com>
2026-04-10 18:24:47 -0300

    modifico las ID de los filtros utilizando el hook useId

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,8 +1,18 @@
 import "./Filters.css";
-import { useState } from "react";
+import { useState, useId } from "react";
 export default function Filters({ onChange }) {
   const [minPrice, setMinPrice] = useState(0);
+  const [priceFilterId] = useId();
+  const [categoryFilterId] = useId();
 
+  /*************  ✨ Windsurf Command ⭐  *************/
+  /**
+   * Handles changes to the minimum price filter.
+   * Updates the local state with the new minimum price
+   * and calls the onChange callback with the updated filter object.
+   * @param {Event} e The event object from the input range change event.
+   */
+  /*******  35ad29f7-0e07-45cd-8053-ba5f031a918d  *******/
   const handleMinPriceChange = (e) => {
     setMinPrice(e.target.value);
     onChange((prev) => ({ ...prev, minPrice: e.target.value }));
@@ -15,7 +25,11 @@ export default function Filters({ onChange }) {
     <section className="filters">
       <div>
         <label htmlFor="category">Categorias</label>
-        <select name="category" id="category" onChange={handleCategoryChange}>
+        <select
+          name="category"
+          id={categoryFilterId}
+          onChange={handleCategoryChange}
+        >
           <option value="all">Todas</option>
           <option value="groceries">Comestibles</option>
           <option value="beauty">Belleza</option>
@@ -27,7 +41,7 @@ export default function Filters({ onChange }) {
         <input
           type="range"
           name="price"
-          id="price"
+          id={priceFilterId}
           value={minPrice}
           min="0"
           max="100"
